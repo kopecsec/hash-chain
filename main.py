16c389c811114e74284d180ba457e75cc74e5312
kopecsec <55878241+kopecsec@users.noreply.github.com>
2020-03-10 21:33:15 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import hashlib # import
+
+text = "ecsc" # nOOB and Noob has cases reversed, so ECSC becomes ecsc
+
+while text!= 'c89aa2ffb9edcc6604005196b5f0e0e4': # compare chain hashes to the one were looking for
+
+    text = hashlib.md5(text.encode()).hexdigest() # 
+
+
+    print(text)
+
